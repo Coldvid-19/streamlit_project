23a29d50d6475dbea48f015409c728fb0dd66514
Nicholas <61555033+Coldvid-19@users.noreply.github.com>
2023-02-13 01:10:06 +0800

    first commit

---
+++ streamlit_tutorial.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="MyFirstStreamlitPage", layout="wide")
+
+st.subheader("Hi, I am Weihao Yeong.")
+st.title("This is a tutorial for streamlit")
+st.write("First webpage for with streamlit.")
